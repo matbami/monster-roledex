99f33263c9906f701d98042388db2e336d7064e9
Ayobami <adelekeayobami13@gmail.com>
2020-09-01 15:50:42 +0100

    Updates

---
+++ precache-manifest.2b969ace106b8f70364a12d327c8536b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e84881b0a5c474f291bd47a5cb8cfcb9",
+    "url": "/monster-roledex/index.html"
+  },
+  {
+    "revision": "ff21e0db79d4900f9864",
+    "url": "/monster-roledex/static/css/main.314b8719.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monster-roledex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-roledex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff21e0db79d4900f9864",
+    "url": "/monster-roledex/static/js/main.797a1a1e.chunk.js"
+  },
+  {
+    "revision": "e2fa00574a0f921fd157",
+    "url": "/monster-roledex/static/js/runtime-main.2c9f2c57.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monster-roledex/static/media/logo.5d5d9eef.svg"
+  }
+]);
